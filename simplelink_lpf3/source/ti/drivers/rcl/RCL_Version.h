1ab2692562035be7afafc8364f19302c8cbcebc8
Vebjorn Myklebust <v.myklebust@ti.com>
2023-08-27 21:29:53 +0200

    simplelink: ti: devices: Add support for CC2340R5
    Add initial lpf3 content to support cc2340r5. Currently, this includes
    files necessary to support power management, and the gpio driver.
    All files are added seperately from lpf2 content, which introduces a new
    folder structure in the TI HAL.
    
    Provide minor bugfixes and updates to content that is more aligned with upstream.
    
    Signed-off-by: Vebjorn Myklebust <v.myklebust@ti.com>
    Signed-off-by: Lars Thalian Morstad <l-morstad@ti.com>
    Signed-off-by: Min Xu <m-xu@ti.com>
    Signed-off-by: Masha Heller <masha.heller@ti.com>
    Signed-off-by: Stoyan Bogdanov <sbogdanov@baylibre.com>
    Signed-off-by: Julien Panis <jpanis@baylibre.com>
    Signed-off-by: Cameron Soviero <cam@ti.com>
    Signed-off-by: Ging Gonzalez <g-gonzalez@ti.com>

---
+++ simplelink_lpf3/source/ti/drivers/rcl/RCL_Version.h
@@ -0,0 +1,15 @@
+
+#ifndef __source_ti_drivers_rcl_rcl_version__
+#define __source_ti_drivers_rcl_rcl_version__
+
+#define RCL_VERSION_MAJOR 0x08
+#define RCL_VERSION_MINOR 0x40
+#define RCL_VERSION_PATCH 0x00
+#define RCL_VERSION_BUILD 0x23
+#define RCL_VERSION_CHANNEL "ga"
+
+#define RCL_VERSION_SHA 0x46599b88ac767a71
+#define RCL_VERSION 0x08400023
+#define RCL_VERSION_CHANNEL_NUMBER 0x1
+
+#endif
