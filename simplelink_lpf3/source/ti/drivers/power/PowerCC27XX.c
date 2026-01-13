24a3beea0afd2527de88979deb76dc7a1c69e0ba
Lars Thalian Morstad <l-morstad@ti.com>
2026-01-13 10:53:55 +0100

    simplelink_lpf3: drivers: power: cc27xx: adapt power driver to zephyr
    Ports zephyr-specific changes from cc23xx power driver.
    
    Signed-off-by: Lars Thalian Morstad <l-morstad@ti.com>

--- simplelink_lpf3/source/ti/drivers/power/PowerCC27XX.c
+++ simplelink_lpf3/source/ti/drivers/power/PowerCC27XX.c
@@ -49,6 +49,8 @@
 
 #include <ti/log/Log.h>
 
+#include <zephyr/pm/policy.h>
+
 #include <ti/devices/DeviceFamily.h>
 #include DeviceFamily_constructPath(inc/hw_types.h)
 #include DeviceFamily_constructPath(inc/hw_memmap.h)
@@ -372,6 +374,8 @@ int_fast16_t Power_init(void)
                      HFXT_AMP_COMP_START_TIMEOUT_US / ClockP_getSystemTickPeriod(),
                      NULL);
 
+    uintptr_t hwiKey = HwiP_disable();
+
     /* Start HFXT */
     PowerCC27XX_startHFXT();
 
@@ -381,6 +385,8 @@ int_fast16_t Power_init(void)
      * is started.
      */
     ClockP_start(&hfxtAmpCompClock);
+
+    HwiP_restore(hwiKey);
 
     /* Enable tracking loop with HFXT as reference. This will automatically
      * calibrate LFOSC against HFXT whenever HFXT is enabled; usually after
@@ -612,6 +618,18 @@ int_fast16_t Power_setConstraint(uint_fast16_t constraintId)
 
     DebugP_assert(constraintId < PowerCC27XX_NUMCONSTRAINTS);
 
+    /* Forward constraint set to Zephyr */
+    switch (constraintId) {
+    case PowerLPF3_DISALLOW_STANDBY:
+        pm_policy_state_lock_get(PM_STATE_STANDBY, PM_ALL_SUBSTATES);
+        break;
+    case PowerLPF3_DISALLOW_IDLE:
+        pm_policy_state_lock_get(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES);
+        break;
+    default:
+        break;
+    }
+
     key = HwiP_disable();
 
     /* Set the specified constraint in the constraintMask for faster access */
@@ -638,6 +656,18 @@ int_fast16_t Power_releaseConstraint(uint_fast16_t constraintId)
     key = HwiP_disable();
 
     DebugP_assert(constraintCounts[constraintId] != 0U);
+
+    /* Forward constraint release to Zephyr */
+    switch (constraintId) {
+    case PowerLPF3_DISALLOW_STANDBY:
+        pm_policy_state_lock_put(PM_STATE_STANDBY, PM_ALL_SUBSTATES);
+        break;
+    case PowerLPF3_DISALLOW_IDLE:
+        pm_policy_state_lock_put(PM_STATE_RUNTIME_IDLE, PM_ALL_SUBSTATES);
+        break;
+    default:
+        break;
+    }
 
     constraintCounts[constraintId]--;
 
