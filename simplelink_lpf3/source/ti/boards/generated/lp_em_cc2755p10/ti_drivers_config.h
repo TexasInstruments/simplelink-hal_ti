60268101a9e0983c2743c90d8d3c35626a077d5c
Eirik Runshaug <e-runshaug@ti.com>
2026-01-16 09:57:28 +0100

    simplelink_lpf3: Add support for CC2755P10
    Add support for CC2755P10 device
    
    Signed-off-by: Eirik Runshaug <e-runshaug@ti.com>

---
+++ simplelink_lpf3/source/ti/boards/generated/lp_em_cc2755p10/ti_drivers_config.h
@@ -0,0 +1,66 @@
+/*
+ *  ======== ti_drivers_config.h ========
+ *  Configured TI-Drivers module declarations
+ *
+ *  The macros defines herein are intended for use by applications which
+ *  directly include this header. These macros should NOT be hard coded or
+ *  copied into library source code.
+ *
+ *  Symbols declared as const are intended for use with libraries.
+ *  Library source code must extern the correct symbol--which is resolved
+ *  when the application is linked.
+ *
+ *  DO NOT EDIT - This file is generated for the LP_EM_CC2755P10
+ *  by the SysConfig tool.
+ */
+#ifndef ti_drivers_config_h
+#define ti_drivers_config_h
+
+#define CONFIG_SYSCONFIG_PREVIEW
+
+#define CONFIG_LP_EM_CC2755P10
+#ifndef DeviceFamily_CC27XX
+#define DeviceFamily_CC27XX
+#endif
+
+#include <ti/devices/DeviceFamily.h>
+
+#include <stdint.h>
+
+/* support C++ sources */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/*
+ *  ======== ECDH ========
+ */
+
+extern const uint_least8_t              CONFIG_ECDH_0_CONST;
+#define CONFIG_ECDH_0                   0
+#define CONFIG_TI_DRIVERS_ECDH_COUNT    1
+
+
+/*
+ *  ======== GPIO ========
+ */
+/* The range of pins available on this device */
+extern const uint_least8_t GPIO_pinLowerBound;
+extern const uint_least8_t GPIO_pinUpperBound;
+
+/* LEDs are active high */
+#define CONFIG_GPIO_LED_ON  (1)
+#define CONFIG_GPIO_LED_OFF (0)
+
+#define CONFIG_LED_ON  (CONFIG_GPIO_LED_ON)
+#define CONFIG_LED_OFF (CONFIG_GPIO_LED_OFF)
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* include guard */
